2c2a83538e5219185e997107923dda6c771e6c8c
jsphbngnrd <hello@josephbongrand.com>
2023-10-18 14:08:08 +0200

    Adding swiper init

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('Hello World! This is Bate');
+
+$(document).ready(function() {
+    console.log("Initializing Swiper...");
+
+    const swiper = new Swiper('.explore_wrapper', {
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+
+    console.log("Swiper initialized.");
+});

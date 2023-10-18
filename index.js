f3395c198210d210a59890d64de755c60c1bb0f9
jsphbngnrd <hello@josephbongrand.com>
2023-10-18 14:13:50 +0200

    Update swiper

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 console.log('Hello World! This is Bate');
 
-$(document).ready(function() {
-    console.log("Initializing Swiper...");
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
 
-    const swiper = new Swiper('.explore_wrapper', {
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-    });
-
-    console.log("Swiper initialized.");
-});
+    loop: true
+  
+  });
